cf96aeb4bc59ff8c543b762b9f4aa1a8174d9fd7
Bruno Capuano <bcapuano@gmail.com>
2026-02-12 21:06:00 -0500

    feat: restructure project files and remove Spectre.Console dependency

--- samples/RagOllama/Program.cs
+++ samples/RagOllama/Program.cs
@@ -7,48 +7,31 @@ using OllamaSharp;
 
 var ollamaEndpoint = "http://localhost:11434";
 var modelIdChat = "phi3.5";
-
-// questions
 var question = "What is Bruno's favourite super hero?";
 
-// intro
-SpectreConsoleOutput.DisplayTitle(modelIdChat);
-SpectreConsoleOutput.DisplayTitleH2($"This program will answer the following question:");
-SpectreConsoleOutput.DisplayTitleH3(question);
-SpectreConsoleOutput.DisplayTitleH2($"Approach:");
-SpectreConsoleOutput.DisplayTitleH3($"1st approach will be to ask the question directly to the {modelIdChat} model.");
-SpectreConsoleOutput.DisplayTitleH3("2nd approach will be to add facts to a semantic memory and ask the question again");
+// --- Ask without memory ---
+Console.WriteLine($"Question: {question}");
+Console.WriteLine($"\n--- {modelIdChat} response (no memory) ---");
 
-SpectreConsoleOutput.DisplayTitleH2($"{modelIdChat} response (no memory).");
+var ollama = new OllamaApiClient(ollamaEndpoint) { SelectedModel = modelIdChat };
+await foreach (var token in ollama.GenerateAsync(question))
+    Console.Write(token.Response);
 
-// set up the client
-var ollama = new OllamaApiClient(ollamaEndpoint)
-{
-    SelectedModel = modelIdChat
-};
+// --- Build Kernel Memory with local embeddings ---
+Console.WriteLine("\n\n--- Importing facts into Kernel Memory ---");
 
-await foreach (var answerToken in ollama.GenerateAsync(question))
-{
-    Console.Write(answerToken.Response.ToString());
-}
-;
-
-// separator
-Console.WriteLine("");
-SpectreConsoleOutput.DisplaySeparator();
-
-var configOllamaKernelMemory = new OllamaConfig
+var config = new OllamaConfig
 {
     Endpoint = ollamaEndpoint,
     TextModel = new OllamaModelConfig(modelIdChat)
 };
 
 var memory = new KernelMemoryBuilder()
-    .WithOllamaTextGeneration(configOllamaKernelMemory)
+    .WithOllamaTextGeneration(config)
     .WithLocalEmbeddings()
     .Build();
 
-var informationList = new List<string>
+var facts = new[]
 {
     "Gisela's favourite super hero is Batman",
     "Gisela watched Venom 3 2 weeks ago",
@@ -58,23 +41,15 @@ var informationList = new List<string>
     "ACE and Goku watched the movies Venom 3 and Eternals",
 };
 
-SpectreConsoleOutput.DisplayTitleH2($"Information List");
-
-int docId = 1;
-foreach (var info in informationList)
+for (var i = 0; i < facts.Length; i++)
 {
-    SpectreConsoleOutput.WriteYellow($"Adding docId: {docId} - information: {info}", true);
-    await memory.ImportTextAsync(info, docId.ToString());
-    docId++;
+    Console.WriteLine($"  [{i + 1}] {facts[i]}");
+    await memory.ImportTextAsync(facts[i], (i + 1).ToString());
 }
 
-SpectreConsoleOutput.DisplayTitleH3($"Asking question with memory: {question}");
-var answer = memory.AskStreamingAsync(question);
-await foreach (var result in answer)
-{
-    SpectreConsoleOutput.WriteGreen($"{result.Result}");
-    SpectreConsoleOutput.DisplayNewLine();
+// --- Ask with memory ---
+Console.WriteLine($"\n--- Asking with memory: {question} ---");
+await foreach (var result in memory.AskStreamingAsync(question))
+    Console.Write(result.Result);
 
-}
-
-Console.WriteLine($"");
+Console.WriteLine();
